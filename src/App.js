4fe109e93dbcd72d0939487747e10e4b2fa23167
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-04-29 12:46:39 -0400

    added sass.

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
       {categories.map(category => {
         return (
           <div className="category-container" key={category.id}>
-            {/* <img src="" alt="" /> */}
+            <div className="background-image"></div>
             <div className="category-body-container">
               <h2>{category.title}</h2>
               <p>Shop Now</p>
