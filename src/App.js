f3fdfe859efae25b4c37345ad2869bdd07d205bc
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-04-29 13:06:12 -0400

    new json file added and images added.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import './categories.styles.scss';
+import categories from './categories';
 
 const App = () => {
-
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats'
-    },
-    {
-      id: 2,
-      title: 'Jackets'
-    },
-    {
-      id: 3,
-      title: 'Sneakers'
-    },
-    {
-      id: 4,
-      title: 'Womens'
-    },
-    {
-      id: 5,
-      title: 'Mens'
-    }
-  ];
 
   return (
     <div className="categories-container">
       {categories.map(category => {
         return (
           <div className="category-container" key={category.id}>
-            <div className="background-image"></div>
+            <div className="background-image" style={{ backgroundImage: `url(${category.imageUrl})` }}></div>
             <div className="category-body-container">
               <h2>{category.title}</h2>
               <p>Shop Now</p>
