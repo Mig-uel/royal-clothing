03132a25a29d85f3a205fbd130c686c48ee6eb4a
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-05-10 17:47:21 -0400

    init firebase.

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAacMtMvF3iUX2SyclRpDWRVgMUU9_r9N0",
+  authDomain: "royal-db-d0279.firebaseapp.com",
+  projectId: "royal-db-d0279",
+  storageBucket: "royal-db-d0279.appspot.com",
+  messagingSenderId: "283550416346",
+  appId: "1:283550416346:web:93ca16f70b588ecaf854e3"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
