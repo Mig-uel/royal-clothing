073f2ba211e2d860fa85ee7753201e3970a405c8
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-05-10 17:22:58 -0400

    initial sign in page and font changed.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 // Components
 import Home from './routes/Home/home.component'
 import Navigation from './routes/Navigation/navigation.component'
+import SignIn from './routes/Sign-In/sign-in.component'
 
 const Shop = () => {
   return <h1>Shop</h1>
@@ -14,6 +15,7 @@ const App = () => {
       <Route exact path='/' element={<Navigation />} >
         <Route index element={<Home />} exact />
         <Route path='shop' element={<Shop />} exact />
+        <Route path='sign-in' element={<SignIn />} exact />
       </Route>
     </Routes>
 
