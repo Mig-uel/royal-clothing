14d00eeaddbc12965e05cc5485ae43abfacf815f
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-07-08 15:02:15 -0400

    lazy loading and suspense implemented.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useEffect, lazy, Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
 // Components
-import Home from './routes/Home/home.component';
-import Navigation from './routes/Navigation/navigation.component';
-import Auth from './routes/auth/auth.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
+import Spinner from './components/spinner/spinner.component'
+import { checkUserSession } from './store/user/user.action'
 
-import { checkUserSession } from './store/user/user.action';
+const Home = lazy(() => import('./routes/Home/home.component'))
+const Auth = lazy(() => import('./routes/auth/auth.component'))
+const Navigation = lazy(() =>
+  import('./routes/Navigation/navigation.component')
+)
+const Shop = lazy(() => import('./routes/shop/shop.component'))
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'))
 
 const App = () => {
-
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(checkUserSession());
-  }, []);
+    dispatch(checkUserSession())
+  }, [])
 
   return (
-    <Routes>
-      <Route exact path='/' element={<Navigation />} >
-        <Route index element={<Home />} exact />
-        <Route path='shop/*' element={<Shop />} exact />
-        <Route path='auth' element={<Auth />} exact />
-        <Route path='checkout' element={<Checkout />} exact />
-      </Route>
-    </Routes>
-  );
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route exact path='/' element={<Navigation />}>
+          <Route index element={<Home />} exact />
+          <Route path='shop/*' element={<Shop />} exact />
+          <Route path='auth' element={<Auth />} exact />
+          <Route path='checkout' element={<Checkout />} exact />
+        </Route>
+      </Routes>
+    </Suspense>
+  )
 }
 
-export default App;
+export default App
